cd9c49ce5af4e1dcb30c4202318ed28379b27042
Barış Karabay <bariskarabay@yandex.com>
2015-03-03 15:11:07 +0200

    seeds.rb ve proje.rb dosyaların da değişiklik yapıldı

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create(title: 'Ruby ile programlama',
+description:
+%{
+	<p>
+	Ruby diger dilleri benzeyen dinamik bir dildir vs vs vs vs 
+	</p>},
+	image_url: 'resim.jpg',
+	price: 49.95)
--- proje.rb
+++ proje.rb
@@ -1,5 +1,11 @@
-#Depot_a
-
-
-
-#Depot_b
+#Depot6
+       #Depot_6a
+             #Ürünler projesinin ana yapısı rails kullanılarak oluşturuldu
+             #Scaffold MVC yapısı oluşturuldu
+             #Products tablosu oluşturuldu bu tablonun içerisinde title,description,image_url ve price alanları oluşturuldu
+             #Migrate işlemi kullanılarak veritabanı kalıcı hale getirildi rake db:migrate
+             #Rails s diyerek sunucumuz başlatıldı ve proje test edildi
+             #seed komutu kullanıldı
+             #/db/seeds.rb dosyasının içeriği değiştirildi burada ilk önce tüm ürün bilgileri silindi daha sonra ise yeni bir ürün eklendi
+       #Depot_6b
+             #
